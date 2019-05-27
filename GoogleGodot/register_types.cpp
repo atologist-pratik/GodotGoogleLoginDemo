c934e3d4e874ad29dccbecd0206adc98ac4afb94
atologist-pratik <atologist.pratikp@gmail.com>
2019-05-26 23:46:01 -0700

    Add files via upload

--- GoogleGodot/register_types.cpp
+++ GoogleGodot/register_types.cpp
@@ -1,9 +1,10 @@
 #include <core/version_generated.gen.h>
-#include "register_types.h"
+#include <core/reference.h>
 #include <core/class_db.h>
 #include <core/engine.h>
+#include "register_types.h"
 #include "ios/GoogleLogin.h"
-#include "core/reference.h"
+
 
 void register_GoogleGodot_types() {
     Engine::get_singleton()->add_singleton(Engine::Singleton("GoogleGodot", memnew(GoogleGodot)));
